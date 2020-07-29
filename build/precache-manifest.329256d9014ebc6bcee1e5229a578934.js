ea2d0ef95fa57c645bd0629b04de426616aed77c
Yi Jiang <jiangyi2010@hotmail.com>
2020-07-29 13:22:20 -0600

    my first commit

---
+++ build/precache-manifest.329256d9014ebc6bcee1e5229a578934.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1043b4be9dad41fa40bf74a714b11b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dc717e3300979282b19",
+    "url": "/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "92b913e020c64d7e92bb",
+    "url": "/static/css/main.e30a3060.chunk.css"
+  },
+  {
+    "revision": "9dc717e3300979282b19",
+    "url": "/static/js/2.4d4965b0.chunk.js"
+  },
+  {
+    "revision": "1af0279a1fb7be8464747233b37e4479",
+    "url": "/static/js/2.4d4965b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b913e020c64d7e92bb",
+    "url": "/static/js/main.d7f13189.chunk.js"
+  },
+  {
+    "revision": "15bdf26677c13ebe6945",
+    "url": "/static/js/runtime-main.876d68fc.js"
+  },
+  {
+    "revision": "bc8cc4eae3d3822562e825fb5cec5536",
+    "url": "/static/media/blue-marker.bc8cc4ea.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
